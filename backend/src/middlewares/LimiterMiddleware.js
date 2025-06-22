d9e06fa83d491337deb8af2d8331a1cd032e95c6
adelson70 <bittencourtadelson1@gmail.com>
2025-06-22 16:03:46 -0300

    limite de requisições por min

---
+++ backend/src/middlewares/LimiterMiddleware.js
@@ -0,0 +1,28 @@
+const rateLimit = require('express-rate-limit');
+
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // Limit each IP to 100 requests per windowMs
+    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+    message: {
+        status: 429,
+        error: 'Muitas requisições, tente novamente mais tarde.'
+    }
+});
+
+const limiterLogin = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 5, // Limit each IP to 5 login requests per windowMs
+    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+    message: {
+        status: 429,
+        error: 'Muitas requisições, tente novamente mais tarde.'
+    }
+});
+
+module.exports = {
+    limiter,
+    limiterLogin
+};
--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,19 +1,21 @@
 const express = require('express');
 const router = express.Router();
 const cookieParser = require('cookie-parser');
+const LimiterMiddleware = require('../middlewares/LimiterMiddleware');
 
 const ExampleMiddleware = require('../middlewares/ExampleMiddleware')
 const UsuarioRoutes = require('./UsuarioRoutes');
 const QrCodeRoutes = require('./QrCodeRoutes');
 const AuthRoutes = require('./AuthRoutes');
 const SalaRoutes = require('./SalaRoutes');
+const { limiter, limiterLogin } = LimiterMiddleware;
 
 router.use(ExampleMiddleware)
 router.use(cookieParser());
-router.use('/refresh', AuthRoutes);
-router.use('/qr-code', QrCodeRoutes);
-router.use('/usuario', UsuarioRoutes);
-router.use('/sala', SalaRoutes);
+router.use('/refresh', limiterLogin, AuthRoutes);
+router.use('/qr-code', limiter, QrCodeRoutes);
+router.use('/usuario', limiterLogin, UsuarioRoutes);
+router.use('/sala', limiter, SalaRoutes);
 
 module.exports = router;
 
